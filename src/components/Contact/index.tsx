43c5c245e8a8f96e746f6a3599550d8af9173652
Darko <darko.stojanovski64@gmail.com>
2023-07-04 15:42:27 +0200

    adding basic files

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+import { FC } from "react";
+
+export const Contact: FC = () => {
+  return <Box>Hello from Contact</Box>;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './About'
+export * from './Contact'
+export * from './Projects'
+export * from './Header'
+export * from './Footer'
